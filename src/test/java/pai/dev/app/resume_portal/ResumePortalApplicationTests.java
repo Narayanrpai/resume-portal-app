c21db42adbdd933ce89a277e24474ccd672d4cfb
Narayan Pai <narayanrpai19@gmail.com>
2024-07-15 19:44:46 +0530

    User and repository JPA added

---
+++ src/test/java/pai/dev/app/resume_portal/ResumePortalApplicationTests.java
@@ -0,0 +1,13 @@
+package pai.dev.app.resume_portal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResumePortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
