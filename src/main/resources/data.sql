ae1a9e32884dd9a576d4ed783cf0ff26af97c163
Narayan Pai <narayanrpai19@gmail.com>
2024-07-19 14:34:01 +0530

    Added basic user profile elements and displayed in templates

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
-INSERT INTO users (id, user_name, password, active, roles) values (1, 'foo', 'foo', true, 'USER');
-INSERT INTO users (id, user_name, password, active, roles) values (2, 'bar', 'bar', true, 'USER');
-INSERT INTO user_profile (id, user_name, theme, summary) values
-(1, 'foo', '1', 'User name foo'),
-(2, 'bar', '2', 'User name bar');
+INSERT INTO users (id, user_name, password, active, roles) values (1, 'einstein', 'einstein', true, 'USER');
+INSERT INTO users (id, user_name, password, active, roles) values (2, 'newton', 'newton', true, 'USER');
+INSERT INTO user_profile (id, user_name, theme, summary, first_name, last_name, email, phone, designation) values
+(1, 'einstein', '1', 'Best known for developing the theory of relativity, Einstein also made important contributions to quantum mechanics.', 'Albert', 'Einstein','einstein@gmail.com','111-111-1111','Theoretical Physicist.'),
+(2, 'newton', '2', ' He was a key figure in the Scientific Revolution and the Enlightenment that followed. His pioneering book Philosophiæ Naturalis Principia Mathematica.', 'Isac', 'Newton','newton@gmail.com','611-121-1441','Mathematician, physicist, astronomer, theologian and author.');
